7373012c4e7fd054170aa1515843ae09e04e33fc
Maria-Gio-437 <giovannamaria437@gmail.com>
2025-06-01 23:09:30 -0300

    Adicionando backend

---
+++ Backend/config/supabaseClient.ts
@@ -0,0 +1,14 @@
+import { createClient } from '@supabase/supabase-js';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../../../.env') }); 
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error('Supabase URL ou Anon Key não estão definidas nas variáveis de ambiente.');
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
