873d42705d5a9b6f2b9e095cf0157ea67a0b154e
Maria-Gio-437 <giovannamaria437@gmail.com>
2025-06-16 16:32:30 -0300

    Correção de teste de conexão com banco de dados

--- Backend/tests/connection_bd.py
+++ Backend/tests/connection_bd.py
@@ -1,30 +1,11 @@
-import os
-import sys
-from dotenv import load_dotenv
-
-# Encontrar os módulos
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# Carregar as variáveis de ambiente do env
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
-
 from config.database import supabase
 
-def check_database_connection():
-    print("Teste de conexão com supabase\n")
+print("Conectando ao supabase\n")
 
-    try:
-        response = supabase.client.from_('usuarios').select('id').limit(1).execute()
+try:
+    response = supabase.table('usuarios').select('id', head=True).limit(1).execute()
 
-        if response.data:
-            print("Conexão com o Supabase bem-sucedida!")
-            print("Dados recebidos:", response.data)
-        else:
-            print("Falha na conexão com o Supabase.")
-            print("Detalhes do erro:", response)
+    print("Conexão com o Supabase bem-sucedida!")
 
-    except Exception as errors:
-        print(f"Ocorreu um erro inesperado ao tentar conectar ao Supabase: {errors}")
-
-if __name__ == "__main__":
-    check_database_connection()
+except Exception as e:
+    print(f"Falha ao conectar ou consultar o Supabase: {e}")
