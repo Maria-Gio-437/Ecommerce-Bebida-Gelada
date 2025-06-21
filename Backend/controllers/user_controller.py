b1213aecffdf9200ec349dcda5b90302f35fb069
Maria-Gio-437 <giovannamaria437@gmail.com>
2025-06-21 10:12:02 -0300

    Correção de login e cadastro

--- Backend/controllers/user_controller.py
+++ Backend/controllers/user_controller.py
@@ -3,13 +3,22 @@ from application.services.user_service import UserService
 from application.dtos.create_user_dto import CreateUserDTO
 from application.dtos.login_dto import LoginDTO
 from pydantic import ValidationError
+from datetime import date, datetime
+
+user_service = UserService()
+
+def serialize_dates(data):
+    return {
+        k: (v.isoformat() if isinstance(v, (date, datetime)) else v)
+        for k, v in data.items()
+    }
 
 def create_user_controller():
     try:
         user_data = CreateUserDTO(**request.json)
         user_service = UserService()
         new_user = user_service.create_user(user_data)
-        return jsonify(new_user), 201
+        return jsonify(serialize_dates(new_user)), 201
     except ValidationError as e:
         return jsonify({"error": e.errors()}), 400
     except ValueError as e:
@@ -22,10 +31,22 @@ def login_user_controller():
         login_data = LoginDTO(**request.json)
         user_service = UserService()
         user = user_service.login_user(login_data)
-        return jsonify(user)
+        return jsonify(serialize_dates(user))
     except ValidationError as e:
         return jsonify({"error": e.errors()}), 400
     except ValueError as e:
         return jsonify({"error": str(e)}), 401
     except Exception as e:
         return jsonify({"error": "An unexpected error occurred", "details": str(e)}), 500
+
+def get_all_users():
+    """Controlador para lidar com a requisição de listar todos os usuários."""
+    try:
+        users = user_service.get_all_users()
+        if users is not None:
+            serialized = [serialize_dates(user) for user in users]
+            return jsonify(serialized), 200
+        else:
+            return jsonify({"message": "Nenhum usuário encontrado ou erro no servidor"}), 404
+    except Exception as e:
+        return jsonify({"message": "Erro interno no servidor", "error": str(e)}), 500
--- Backend/routes/user_routes.py
+++ Backend/routes/user_routes.py
@@ -1,8 +1,10 @@
 from flask import Blueprint
-from controllers.user_controller import create_user_controller, login_user_controller
+from controllers.user_controller import create_user_controller, login_user_controller, get_all_users
 
 user_bp = Blueprint('user_bp', __name__)
 
 user_bp.route('/register', methods=['POST'])(create_user_controller)
 
 user_bp.route('/login', methods=['POST'])(login_user_controller)
+
+user_bp.route('/', methods=['GET'])(get_all_users)
